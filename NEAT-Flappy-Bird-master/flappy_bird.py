64670068d64701f30ff047f59df3bd4dcfa86219
Ngô Quang Thịnh - k64 <96581876+ThinhNgo123@users.noreply.github.com>
2022-10-04 16:13:09 +0700

    a

--- NEAT-Flappy-Bird-master/flappy_bird.py
+++ NEAT-Flappy-Bird-master/flappy_bird.py
@@ -25,10 +25,10 @@ DRAW_LINES = False
 WIN = pygame.display.set_mode((WIN_WIDTH, WIN_HEIGHT))
 pygame.display.set_caption("Flappy Bird")
 
-pipe_img = pygame.transform.scale2x(pygame.image.load(os.path.join("imgs","pipe.png")).convert_alpha())
-bg_img = pygame.transform.scale(pygame.image.load(os.path.join("imgs","bg.png")).convert_alpha(), (600, 900))
-bird_images = [pygame.transform.scale2x(pygame.image.load(os.path.join("imgs","bird" + str(x) + ".png"))) for x in range(1,4)]
-base_img = pygame.transform.scale2x(pygame.image.load(os.path.join("imgs","base.png")).convert_alpha())
+pipe_img = pygame.transform.scale2x(pygame.image.load("D:\Data\Project_company\python_project\\NEAT-Flappy-Bird-master\imgs\pipe.png").convert_alpha())
+bg_img = pygame.transform.scale(pygame.image.load("D:\Data\Project_company\python_project\\NEAT-Flappy-Bird-master\imgs\\bg.png").convert_alpha(), (600, 900))
+bird_images = [pygame.transform.scale2x(pygame.image.load("D:\Data\Project_company\python_project\\NEAT-Flappy-Bird-master\imgs\\bird" + str(x) + ".png")) for x in range(1,4)]
+base_img = pygame.transform.scale2x(pygame.image.load("D:\Data\Project_company\python_project\\NEAT-Flappy-Bird-master\imgs\\base.png").convert_alpha())
 
 gen = 0
 
